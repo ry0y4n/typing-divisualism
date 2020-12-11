bea037e6d0bd0721e44088f43337cadfda131d39
ry0y4n <rusk.ryoy4n@gmail.com>
2020-12-12 01:21:41 +0900

    add: トップページの簡易実装

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,7 +47,8 @@ export default {
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module',
-    '@nuxtjs/vuetify'
+    '@nuxtjs/vuetify',
+    '@nuxtjs/tailwindcss'
   ],
   /*
   ** Nuxt.js modules
--- pages/index.vue
+++ pages/index.vue
@@ -1,50 +1,63 @@
 <template>
-  <div class="container">
-    <div>
-      <h1 class="title">
-        divisualism
-      </h1>
+  <div>
+    <div class="shutter"></div>
+    <div class="container">
       <div>
-        <p>悪い奴</p>
-        <h4>{{ badText }}</h4>
-        <textarea ref="badguy" :value="badText" placeholder="be a bad guy" @keydown="trigger" />
-      </div>
-      <div>
-        <p>良い奴</p>
-        <h4>{{ goodText }}</h4>
-        <textarea ref="goodguy" :value="goodText" placeholder="be a good guy" @keydown="trigger" />
-      </div>
-      <div>
-        <p>変な奴</p>
-        <h4>{{ weirdText }}</h4>
-        <textarea ref="weirdguy" :value="weirdText" placeholder="be a weird guy" @keydown="trigger" />
-      </div>
-      <vue-typed-js :strings="['^1000 分人主義 ^500 それは苦しい']">
-        <h1 class="typing" />
-      </vue-typed-js>
-      <vue-typed-js :strings="['〇〇と言いますが何か？']">
-        <h1>自己紹介 <span class="typing" /></h1>
-      </vue-typed-js>
-      <div class="links">
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
+        <vue-typed-js :strings="['^2000IVISUALISM']" :type-speed="200" :show-cursor="false">
+          <h1 class="title">
+            D<span class="typing" />
+          </h1>
+        </vue-typed-js>
+        <div>
+          <p class="guys">悪い奴</p>
+          <textarea
+            ref="badguy"
+            rows="5"
+            cols="50"
+            :value="badText"
+            placeholder="Be a 'bad' guy"
+            @keydown="trigger"
+          />
+        </div>
+        <div>
+          <p class="guys">良い奴</p>
+          <textarea
+            ref="goodguy"
+            rows="5"
+            cols="50"
+            :value="goodText"
+            placeholder="Be a 'good' guy"
+            @keydown="trigger"
+          />
+        </div>
+        <div>
+          <p class="guys">変な奴</p>
+          <textarea
+            ref="weirdguy"
+            rows="5"
+            cols="50"
+            :value="weirdText"
+            placeholder="Be a 'weird' guy"
+            @keydown="trigger"
+          />
+        </div>
+        <!-- <vue-typed-js :strings="['^1000 分人主義 ^500 それは苦しい']">
+          <h1 class="typing" />
+        </vue-typed-js>
+        <vue-typed-js :strings="['〇〇と言いますが何か？']">
+          <h1>自己紹介 <span class="typing" /></h1>
+        </vue-typed-js>
+        <button @click="store">
+          ストアへ保存
+        </button>
+        <button @click="log">
+          ストアを出力
+        </button> -->
+        <a class="btn" @click="routePlayPage">
+          <span>プレイページへ</span>
         </a>
       </div>
     </div>
-    <button @click="store">
-      ストアへ保存
-    </button>
-    <button @click="log">
-      ストアを出力
-    </button>
-    <button @click="routePlayPage">
-      プレイページへ
-    </button>
   </div>
 </template>
 
@@ -171,7 +184,72 @@ export default Vue.extend({
 })
 </script>
 
-<style>
+<style lang="scss">
+.shutter {
+  position:fixed;
+  top:0;
+  left:0;
+  right:0;
+  bottom:0;
+  background-color:#1e1e1e;
+  z-index:9999;
+
+  -webkit-animation: byeShutter 2.6s forwards;
+          animation: byeShutter 2.6s forwards;
+}
+.shutter::before {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  margin: auto;
+  background-color: #fff;
+  width: 0;
+  height: 1px;
+
+  -webkit-animation: shutterOpen 2.6s forwards;
+          animation: shutterOpen 2.6s forwards;
+}
+@keyframes byeShutter {
+  70% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+    display: none;
+    z-index: -1;
+  }
+}
+@keyframes shutterOpen {
+  0% {
+    width: 0;
+    height: 1px;
+  }
+  50% {
+    width: 100%;
+    height: 1px;
+  }
+  90% {
+    width: 100%;
+    height: 100%;
+  }
+  100% {
+    width: 100%;
+    height: 100%;
+  }
+}
+@keyframes contentScale {
+  70% {
+    -webkit-transform: perspective(800px) scale(0.9) rotateX(15deg);
+            transform: perspective(800px) scale(0.9) rotateX(15deg);
+  }
+  100% {
+    -webkit-transform: perspective(800px) scale(1) rotateX(0);
+            transform: perspective(800px) scale(1) rotateX(0);
+  }
+}
+
 .container {
   margin: 0 auto;
   min-height: 100vh;
@@ -179,24 +257,111 @@ export default Vue.extend({
   justify-content: center;
   align-items: center;
   text-align: center;
-}
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+  .title {
+    width: 100%;
+    font-family:
+      'Quicksand',
+      'Source Sans Pro',
+      -apple-system,
+      BlinkMacSystemFont,
+      'Segoe UI',
+      Roboto,
+      'Helvetica Neue',
+      Arial,
+      sans-serif;
+    display: block;
+    font-weight: 300;
+    font-size: 100px;
+    color: #35495e;
+    letter-spacing: 1px;
+  }
+
+  .guys {
+    font-size: 24px;
+    margin-top: 25px;
+    margin-bottom: 5px;
+    padding-left: 20px;
+    letter-spacing: 5px;
+  }
+
+  textarea {
+    border: solid 2px;
+    border-radius: 5px;
+    font-size: 30px;
+    width: 100%;
+  }
+  textarea::placeholder {
+    color: #FCE2B1;
+  }
+  textarea::-webkit-resizer {
+      display: none;
+  }
+
+  .btn {
+    margin: 50px 0 100px;
+    display: inline-block;
+    position: relative;
+    width: 160px;
+    height: 50px;
+    line-height:50px;
+    text-align: center;
+    text-decoration: none;
+    color: #1B1B1B;
+    background: #fff;
+
+    span::before {
+      display: inline-block;
+      content: "";
+      width: 10px;
+      height: 10px;
+      position: absolute;
+      bottom: 0px;
+      left: 0px;
+      border-bottom: 1px solid #1B1B1B;
+      border-left: 1px solid #1B1B1B;
+    }
+    span::after {
+      display: inline-block;
+      content: "";
+      width: 10px;
+      height: 10px;
+      position: absolute;
+      bottom: 0px;
+      right: 0px;
+      border-bottom: 1px solid #1B1B1B;
+      border-right: 1px solid #1B1B1B;
+    }
+  }
+  .btn::before {
+    display: inline-block;
+    content: "";
+    width: 10px;
+    height: 10px;
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    border-top: 1px solid #1B1B1B;
+    border-left: 1px solid #1B1B1B;
+  }
+  .btn::after {
+    display: inline-block;
+    content: "";
+    width: 10px;
+    height: 10px;
+    position: absolute;
+    top: 0px;
+    right: 0px;
+    border-top: 1px solid #1B1B1B;
+    border-right: 1px solid #1B1B1B;
+  }
+  .btn:hover {
+    cursor: pointer;
+    text-decoration: none;
+    transition: transform .3s;
+    transform-origin: center;
+    transform: scale(1.05, 1.05);
+  }
 }
 
 .subtitle {
